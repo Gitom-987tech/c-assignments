cd56cff1f1534072a9f8b64997c41935fedc3d5b
GITOM-987TECH <gichohit7@gmail.com>
2024-09-29 12:28:20 +0300

    change of code

--- simple.c
+++ simple.c
@@ -10,29 +10,32 @@ int main () {
 	
 	int time;
 	float rate;
-	double amount, simpleintrest;
-	char string;
+	double amount;
+	double simpleintrest;
+	
 	
 	
 	printf("Enter time\n: ");
 	printf("Enter rate\n: ");
 	printf("Enter amount\n: ");
 	printf("Enter simple intrest\n: ");
-	printf("Enter string\n: ");
+	//calculate simple intrest
+	simpleintrest = (amount * time * rate)/100;
+	
 	
 	scanf("%d", &time);
 	scanf("%f", &rate);
 	scanf("%lf", &amount);
 	scanf("%lf", &simpleintrest);
-	scanf("%s", string);
+	
 	
 	
 	printf("\nYou entered\n");
 	printf("integer: %d\n", time);
 	printf("float: %.2f\n", rate);
 	printf("double: %.2lf\n", amount);
-	printf("double: %.2lf\n", simpleintrest);
-	printf("string: %s\n", string);
+	printf("simple intrest is: %.2lf", simpleintrest);
+	
 	return 0;
 	
 	
